5737d6ca9aaa713381557d0accc2cf1fb14a8668
Booge2 <vladky54@gmail.com>
2024-03-12 19:37:45 +0200

    Appdate cod in file

---
+++ .venv/main.py
@@ -0,0 +1,11 @@
+class Dogs:
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+
+my_dog = Dogs("Барс", 7, "Дворняга")
+print(my_dog.name)
+print(my_dog.age)
+print(my_dog.breed)
